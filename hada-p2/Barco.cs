a2dbe743cf81883a153c1a3873c9e1f45c903dd3
Alejandro <lpratsl.75@gmail.com>
2026-02-27 18:46:33 +0100

    55177138T Clase Barco creada

---
+++ hada-p2/Barco.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    internal class Barco
+    {
+        public Dictionary<Coordenada, String> CoordenadasBarco {  get; private set; }
+
+        public String Nombre { get; set; }
+
+        public int NumDanyos { get; set; }
+
+        public Barco(string nombre, int longitud, char
+        orientacion, Coordenada coordenadaInicio)
+        {
+            Nombre = nombre;
+            
+        }
+
+
+    }
+}
