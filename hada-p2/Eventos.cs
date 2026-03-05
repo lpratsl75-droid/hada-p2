847c625da53b35555284c3c9e4cefe4ca260251b
Alejandro <lpratsl.75@gmail.com>
2026-03-05 20:17:28 +0100

    55177138T Eventos creada

---
+++ hada-p2/Eventos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hada;
+
+namespace Hada
+{
+    internal class TocadoArgs : EventArgs
+    {
+        public String nombre { get; set; }
+        public Coordenada coordenadaImpacto { get; set;}
+
+        public TocadoArgs(string nombre, Coordenada coordenadaImpacto )
+        {
+            this.nombre = nombre;
+            this.coordenadaImpacto = coordenadaImpacto;
+        }
+    }
+
+    internal class HundidoArgs : EventArgs
+    {   
+        public String nombre { get;  set; }
+
+        public HundidoArgs(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+    }
+}
