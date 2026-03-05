87930455c23dd2bcee7ff6161509508f5170219b
Alejandro <lpratsl.75@gmail.com>
2026-03-05 20:15:10 +0100

    55177138T Clase Barco actualizada

--- hada-p2/Barco.cs
+++ hada-p2/Barco.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hada;
 
 namespace Hada
 {
@@ -14,13 +15,80 @@ namespace Hada
 
         public int NumDanyos { get; set; }
 
+        public event EventHandler<TocadoArgs> eventoTocado;
+        public event EventHandler<HundidoArgs> eventoHundido;
+
         public Barco(string nombre, int longitud, char
         orientacion, Coordenada coordenadaInicio)
         {
+
+            CoordenadasBarco = new Dictionary<Coordenada, string>();
+
+            if (orientacion == 'h')
+            {
+                for (int i = 0; i < longitud; i++)
+                {
+                    CoordenadasBarco.Add(new Coordenada(coordenadaInicio.Fila , coordenadaInicio.Columna + i), nombre);
+
+                }
+            }
+            if (orientacion == 'v')
+            {
+                for (int i = 0; i < longitud; i++)
+                {
+                    CoordenadasBarco.Add(new Coordenada(coordenadaInicio.Fila + i , coordenadaInicio.Columna), nombre);
+
+                }
+            }
             Nombre = nombre;
-            
+            NumDanyos = 0;
         }
 
+        public void Disparo(Coordenada c)
+        {
+            if (CoordenadasBarco.ContainsKey(c))
+            {
+                if (!CoordenadasBarco[c].EndsWith("_T"))
+                {
+                    CoordenadasBarco[c] += "_T";
+                    this.NumDanyos++;
+                }
 
+                if(eventoTocado != null)
+                {
+                    eventoTocado(this, new TocadoArgs(CoordenadasBarco[c], c));
+                }
+
+                if (NumDanyos == CoordenadasBarco.Count() && eventoHundido != null)
+                {
+                    eventoHundido(this, new HundidoArgs(CoordenadasBarco[c]));
+                }
+            }
+        }
+
+        public bool hundido()
+        {
+            bool derrivado = true;
+
+            foreach (String nom in CoordenadasBarco.Values)
+            {
+                if(nom == Nombre)
+                {
+                    derrivado = false;
+                }
+            }
+            return derrivado;
+        }
+        public override string ToString()
+        {
+            string resultado = "[" + Nombre + "] - DAÑOS: [" + NumDanyos + "] - HUNDIDO: [" + hundido() + "] - COORDENADAS: ";
+
+            foreach (var par in CoordenadasBarco)
+            {
+                resultado = resultado + "[" + par.Key.ToString() + " :" + par.Value + "]";
+            }
+
+            return resultado.;
+        }
     }
 }
